b739568f438038387a3bc856e98053a9decb2685
Gustavo <gustavo326projetos@gmail.com>
2023-03-16 18:11:11 -0300

    Finalizado header, iniciando os cards

--- app/src/main/java/com/example/interfacelogin/SignInActivity.kt
+++ app/src/main/java/com/example/interfacelogin/SignInActivity.kt
@@ -1,21 +1,28 @@
 package com.example.interfacelogin
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -35,6 +42,10 @@ class SignInActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun InterfaceSignIn(){
+    var search by rememberSaveable{
+        mutableStateOf("")
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Column(modifier = Modifier.fillMaxSize()) {
             Column(modifier = Modifier.fillMaxWidth()) {
@@ -44,7 +55,7 @@ fun InterfaceSignIn(){
                         contentDescription ="",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .size(230.dp)
+                            .size(225.dp)
                     )
                     Column(
                         modifier = Modifier
@@ -90,6 +101,143 @@ fun InterfaceSignIn(){
                                 fontWeight = FontWeight.Bold
                             )
 
+                        }
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Row(modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Start) {
+                            Text(text = stringResource(id = R.string.mytrips_categories),
+                            modifier = Modifier.padding(top = 20.dp, start = 20.dp),
+                            fontSize = 20.sp,
+                                color = Color(86,84,84)
+                                )
+                            }
+                            Row(modifier = Modifier.fillMaxWidth()) {
+                                Card(
+                                    modifier = Modifier
+                                        .size(116.dp, 70.dp)
+                                        .padding(top = 10.dp, start = 20.dp),
+                                    shape = RoundedCornerShape(
+                                        topStart = 8.dp,
+                                        topEnd = 8.dp,
+                                        bottomStart = 8.dp,
+                                        bottomEnd = 8.dp
+                                    ),
+                                    backgroundColor = Color(
+                                        red = 207,
+                                        green = 6,
+                                        blue = 240
+                                    )
+
+                                ) {
+                                    Column(modifier = Modifier.fillMaxWidth(),
+                                        verticalArrangement = Arrangement.Center,
+                                        horizontalAlignment = Alignment.CenterHorizontally) {
+                                        Image(painter = painterResource(id = R.drawable.montain),
+                                            contentDescription = "",
+                                            modifier = Modifier.size(width = 32.dp, height = 32.dp)
+                                        )
+                                        
+                                        Text(
+                                            text = stringResource(id = R.string.mytrips_title_montain),
+                                            fontSize = 14.sp,
+                                            color = Color.White
+
+                                        )
+                                    }
+                                }
+                                Card(
+                                    modifier = Modifier
+                                        .size(116.dp, 70.dp)
+                                        .padding(top = 10.dp, start = 20.dp),
+                                    shape = RoundedCornerShape(
+                                        topStart = 8.dp,
+                                        topEnd = 8.dp,
+                                        bottomStart = 8.dp,
+                                        bottomEnd = 8.dp
+                                    ),
+                                    backgroundColor = Color(
+                                        red = 234,
+                                        green = 171,
+                                        blue = 244
+                                    )
+
+                                ) {
+                                    Column(modifier = Modifier.fillMaxWidth(),
+                                        verticalArrangement = Arrangement.Center,
+                                        horizontalAlignment = Alignment.CenterHorizontally) {
+                                        Image(painter = painterResource(id = R.drawable.snow),
+                                            contentDescription = "",
+                                            modifier = Modifier.size(width = 32.dp, height = 32.dp)
+                                        )
+
+                                        Text(
+                                            text = stringResource(id = R.string.mytrips_title_snow),
+                                            fontSize = 14.sp,
+                                            color = Color.White
+
+                                        )
+                                    }
+                                }
+                                Card(
+                                    modifier = Modifier
+                                        .size(116.dp, 70.dp)
+                                        .padding(top = 10.dp, start = 20.dp),
+                                    shape = RoundedCornerShape(
+                                        topStart = 8.dp,
+                                        topEnd = 8.dp,
+                                        bottomStart = 8.dp,
+                                        bottomEnd = 8.dp
+                                    ),
+                                    backgroundColor = Color(
+                                        red = 234,
+                                        green = 171,
+                                        blue = 244
+                                    )
+
+                                ) {
+                                    Column(modifier = Modifier.fillMaxWidth(),
+                                        verticalArrangement = Arrangement.Center,
+                                        horizontalAlignment = Alignment.CenterHorizontally) {
+                                        Image(painter = painterResource(id = R.drawable.beach),
+                                            contentDescription = "",
+                                            modifier = Modifier.size(width = 32.dp, height = 32.dp)
+                                        )
+
+                                        Text(
+                                            text = stringResource(id = R.string.mytrips_title_beach),
+                                            fontSize = 14.sp,
+                                            color = Color.White
+
+                                        )
+                                    }
+                                }
+                            }
+
+                            OutlinedTextField(
+                                value = search,
+                                onValueChange = {
+                                    Log.i("Smartphone", it)
+                                    search = it
+                                },
+                                label = { Text(text = stringResource(id = R.string.mytrips_search)) },
+                                modifier = Modifier
+                                    .padding(
+                                        start = 23.dp,
+                                        top = 33.dp,
+                                        end = 40.dp
+                                    )
+                                    .fillMaxWidth(),
+                                shape = RoundedCornerShape(16.dp),
+                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                                leadingIcon = {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.search),
+                                        contentDescription = "",
+                                        modifier = Modifier
+                                            .size(26.dp)
+                                    )
+                                },
+                            )
                         }
                     }
                 }
